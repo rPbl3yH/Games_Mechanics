8b92e46516a87cbf684d5548712c7dfa94ade6fd
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-08-01 13:41:18 +0500

    feat: add run state

--- Assets/StateMachine/States/RunState.cs
+++ Assets/StateMachine/States/RunState.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using AtomicHomework.Hero;
+using Declarative;
 
 namespace StateMachine
 {
     [Serializable]
     public class RunState : IState
     {
+        private StateMachine _stateMachine;
+        private MoveSection _moveSection;
+        
+        [Construct]
+        public void Construct(MoveSection moveSection, HeroStates states)
+        {
+            _stateMachine = states.StateMachine;
+            _moveSection = moveSection;
+        }
+        
         public void Enter()
         {
-            
+            _moveSection.OnMoveFinished += OnMoveFinished;
         }
 
         public void Exit()
         {
+            _moveSection.OnMoveFinished -= OnMoveFinished;
+        }
+        
+        private void OnMoveFinished()
+        {
+            _stateMachine.SwitchState(HeroStateType.Idle);
         }
     }
 }
