9e3b5a43d84cd5f507b51565b79f2a2c8c156c05
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-08-09 17:08:15 +0500

    feat: add get and set

--- Assets/Game/TimeReward.cs
+++ Assets/Game/TimeReward.cs
@@ -1,33 +1,43 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Elementary;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class TimeReward : MonoBehaviour
+namespace Game
 {
-    [SerializeField] private int _rewardMoney = 100;
-    [SerializeField] private float _timeToReceive = 5f;
-
-    [ShowInInspector, ReadOnly]
-    private Timer _timer = new();
-
-    private void Start()
+    public class TimeReward : MonoBehaviour
     {
-        _timer.Duration = _timeToReceive;
-        _timer.OnFinished += ReceiveReward;
-    }
+        [SerializeField] private int _rewardMoney = 100;
+        [SerializeField] private float _timeToReceive = 5f;
 
-    [Button]
-    public void Run()
-    {
-        _timer.ResetTime();
-        _timer.Play();
-    }
+        [ShowInInspector, ReadOnly]
+        private Timer _timer = new();
+
+        private void Start()
+        {
+            _timer.Duration = _timeToReceive;
+            _timer.OnFinished += ReceiveReward;
+        }
+
+        public float GetCurrentProgress()
+        {
+            return _timer.Progress;
+        }
+
+        public void SetTimeProgress(float progress)
+        {
+            _timer.Progress = progress;
+        }
+
+        [Button]
+        public void Run()
+        {
+            _timer.ResetTime();
+            _timer.Play();
+        }
     
-    private void ReceiveReward()
-    {
-        Debug.Log("Get reward " +_rewardMoney);
+        private void ReceiveReward()
+        {
+            Debug.Log("Get reward " +_rewardMoney);
+        }
     }
 }
