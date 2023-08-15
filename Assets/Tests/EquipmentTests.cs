d74da1f3f18ca759129bf49fbf1927e702d984cf
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-08-15 12:36:13 +0500

    fix: equipment config

---
+++ Assets/Tests/EquipmentTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    [TestFixture]
+    public class EquipmentTests : MonoBehaviour
+    {
+
+        [Test]
+        public void SampleTest()
+        {
+            
+        }
+    }
+}
