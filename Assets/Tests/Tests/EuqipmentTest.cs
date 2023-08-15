f5ad75d34d0cd696770f1c41b96bbbd953adefb3
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-08-15 18:55:21 +0500

    feat: add simple test

---
+++ Assets/Tests/Tests/EuqipmentTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class EuqipmentTest
+{
+    [UnityTest]
+    public IEnumerator EuqipmentTestWithEnumeratorPasses()
+    {
+        
+        yield return null;
+    }
+}
