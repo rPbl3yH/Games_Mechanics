5435a28a521740ce3f6d0458d9aeb05b9ec596f5
rPbl3yH <tnv2205@gmail.com>
2024-01-03 11:45:34 +0300

    feat: add example script

---
+++ Assets/Scripts/AddressableScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class AddressableScript : MonoBehaviour
+{
+    [ShowInInspector]
+    private ComponentReference<ItemsLayout> _itemLayout;
+
+
+}
